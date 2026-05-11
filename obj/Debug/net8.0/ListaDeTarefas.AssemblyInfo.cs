4b09766a380b5ed5ccf46d3060f99535756b1f12
Hiroshi <elder.pereira@aluno.senai.br>
2026-05-11 16:39:42 -0300

    Update ListaDeTarefas.AssemblyInfo.cs

--- obj/Debug/net8.0/ListaDeTarefas.AssemblyInfo.cs
+++ obj/Debug/net8.0/ListaDeTarefas.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ListaDeTarefas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b566532833d4b088ab099269cc5e9df9d232545")]
 [assembly: System.Reflection.AssemblyProductAttribute("ListaDeTarefas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ListaDeTarefas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
